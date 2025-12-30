e3245c6db23c404140c6c4b586c2fb6cd6e5341d
Uttam M <156332180+Uttam-38@users.noreply.github.com>
2025-12-29 17:36:50 -0700

    Add foreign key constraints to submissions and comments

---
+++ relational-db-query-optimization/sql/create_relations.sql
@@ -0,0 +1,24 @@
+ALTER TABLE submissions
+ADD CONSTRAINT fk_submissions_author
+FOREIGN KEY (author)
+REFERENCES authors(name);
+
+ALTER TABLE submissions
+ADD CONSTRAINT fk_submissions_subreddit
+FOREIGN KEY (subreddit_id)
+REFERENCES subreddits(name);
+
+ALTER TABLE comments
+ADD CONSTRAINT fk_comments_author
+FOREIGN KEY (author)
+REFERENCES authors(name);
+
+ALTER TABLE comments
+ADD CONSTRAINT fk_comments_subreddit
+FOREIGN KEY (subreddit)
+REFERENCES subreddits(display_name);
+
+ALTER TABLE comments
+ADD CONSTRAINT fk_comments_subreddit_id
+FOREIGN KEY (subreddit_id)
+REFERENCES subreddits(name);
